f4386a39756266cad38e384c131b313fbd903a79
Christiaan Roelevld <croelevel@gmail.com>
2015-02-13 17:48:11 +0100

    first commit

---
+++ TheMagicButton.lua
@@ -0,0 +1,31 @@
+targetHost = "82.157.195.146"
+targetPort = 3000
+
+gpio.mode(3, gpio.INPUT, gpio.PULLUP)
+gpio.mode(4, gpio.OUTPUT)
+gpio.write(4,0)
+
+gpio.trig(3, "up", function (state)     
+     tmr.delay(10000)
+     if (gpio.read(3) == 1) then
+          tmr.delay(10000)
+          if (gpio.read(3) == 1) then          
+               print ("pushed")
+               gpio.write(4,0)
+               --if (sk ~= nil) then sk:close() end
+               sk=net.createConnection(net.TCP,0) 
+               sk:on("receive", function(sck, c) 
+                    print (c)
+                    gpio.write(4,1)
+               end)
+             
+               local httpPost = "POST /magicbutton?apikey=xxr06dalzv HTTP/1.1\r\nHost: "..targetHost.."\r\nConnection: close\r\nContent-Type: application/json\r\nContent-Length: 20\r\n\r\n{\"event\": \"pressed\"}"
+               --print("Posting Message:\r\n------------\r\n"..httpPost.."\r\n----------\r\n")
+               sk:connect(targetPort, targetHost)
+               sk:send(httpPost)
+          end
+     end
+end)
+
+gpio.write(4,1)
+print("Waiting for the magic to happen")
---
+++ init.lua
@@ -0,0 +1,16 @@
+gpio.mode(4, gpio.OUTPUT)
+gpio.write(4,0)
+
+print("set up wifi mode")
+wifi.setmode(wifi.STATION)
+wifi.sta.config("crmobile","Crm0bile")
+wifi.sta.connect()
+tmr.alarm(1, 1000, 1, function() 
+    if wifi.sta.getip()== nil then 
+         print("IP unavaiable, Waiting...") 
+    else 
+     tmr.stop(1)
+     print("Config done, IP is "..wifi.sta.getip())
+     dofile("magicbutton.lua")
+    end 
+ end)
