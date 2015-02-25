0cae261fa23113126ef305717ad26dd2f1e93697
vChrisR <croelevel@gmail.com>
2015-02-25 23:06:32 +0100

    Added retry on connection

--- TheMagicButton.lua
+++ TheMagicButton.lua
@@ -6,7 +6,9 @@ gpio.mode(3, gpio.INPUT, gpio.PULLUP)
 gpio.mode(4, gpio.OUTPUT)
 gpio.write(4,0)
 
-gpio.trig(3, "up", function (state)          
+gpio.trig(3, "up", function (state)   
+     isSent = false
+
      tmr.delay(10000)
      if (gpio.read(3) == 1) then
           tmr.delay(10000)
@@ -24,10 +26,19 @@ gpio.trig(3, "up", function (state)
                end)
              
                sk:on("connection", function(sck)
+                    print("Connected. Now Sending...")
                     sk:send(httpPost)
+                    isSent = true
                end)
                
                sk:connect(targetPort, targetHost)               
+
+               tmr.alarm(1, 1000, 0, function() 
+                    if not isSent then
+                         print("retry")
+                         --do retry here
+                    end
+               end)
           end
      end
 end)
